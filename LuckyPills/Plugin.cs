b7dea243ef5d7b60a3e73a541290e0758a774227
Build <jwbradley2003@gmail.com>
2022-03-12 13:59:04 -0500

    Right I broke that

--- LuckyPills/Plugin.cs
+++ LuckyPills/Plugin.cs
@@ -31,7 +31,7 @@ namespace LuckyPills
         public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
 
         /// <inheritdoc />
-        public override Version Version { get; } = new Version(1, 1, 0);
+        public override Version Version { get; } = new Version(2, 0, 0);
 
         /// <inheritdoc />
         public override void OnEnabled()
